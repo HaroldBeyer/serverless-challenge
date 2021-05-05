85bd461194603765b15ea109ceeb6eb562074b22
Haroldo <haroldobeyerbacellar@gmail.com>
2021-05-04 21:12:37 -0300

    Responses

--- handlers/employee.js
+++ handlers/employee.js
@@ -1,46 +1,37 @@
 'use strict';
 
+const { HTTP_SUCCESS } = require('../utils/enums');
+const { httpReturn } = require('../utils/httpReturn');
+
 module.exports.getEmployee = async (event) => {
-    return {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ "message": "Hello World!" })
-    };
+    return httpReturn(
+        HTTP_SUCCESS.SuccessOK,
+        "Hello world",
+        null
+    );
 };
 
 module.exports.insertEmployee = async (event) => {
-    return {
-        statusCode: 201,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ "message": "Hello World!" })
-    };
+    return httpReturn(
+        HTTP_SUCCESS.SuccessCreated,
+        "Hello world",
+        null
+    );
 };
 
 module.exports.updateEmployee = async (event) => {
-    return {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ "message": "Hello World!" })
-    };
+    return httpReturn(
+        HTTP_SUCCESS.SuccessOK,
+        "Hello world",
+        null
+    );
 };
 
 module.exports.deleteEmployee = async (event) => {
-    return {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ "message": "Hello World!" })
-    };
+    return httpReturn(
+        HTTP_SUCCESS.SuccessOK,
+        "Hello world",
+        null
+    );
 };
 
