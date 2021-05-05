0d32e3f1d721aff2e5ca4832792f72d1c006f276
Haroldo <haroldobeyerbacellar@gmail.com>
2021-05-05 06:36:05 -0300

    Working application

--- handlers/employee-handler.js
+++ handlers/employee-handler.js
@@ -11,6 +11,12 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 const TableName = DYNAMO_DB_CONFIGS.TableName;
 
+/**
+ * 
+ * @param {pathParameters.employeeId} event 
+ * @returns httpReturn(statusCode, message, event)
+ * Get a single employee based on employeeId
+ */
 module.exports.getEmployee = async (event) => {
     const employeeId = event.pathParameters.employeeId;
 
@@ -29,19 +35,26 @@ module.exports.getEmployee = async (event) => {
 
 /**
  * 
- * @param {event} event 
+ * @param {} event 
  * @returns httpReturn(statusCode, message, event)
+ * Get all employees from the Stefanini table
  */
 module.exports.getAllEmployees = async (event) => {
     const result = await dynamoDb.scan({ TableName }).promise();
 
     return httpReturn(
         HTTP_SUCCESS.SuccessOK,
-        result,
-        event
+        `Succesfully got all employees`,
+        result
     );
 };
 
+/**
+ * 
+ * @param {age, name, position} event 
+ * @returns httpReturn(statusCode, message, event)
+ * Insert a employee
+ */
 module.exports.insertEmployee = async (event) => {
     const requestBody = JSON.parse(event.body);
     const employeeId = uuid.v1();
@@ -68,23 +81,39 @@ module.exports.insertEmployee = async (event) => {
     );
 };
 
+/**
+ * 
+ * @param {pathParameters.employeeId, body.age, body.name, body.position} event 
+ * @returns httpReturn(statusCode, message, event)
+ * Updates a employee (all properties) based on employeeId
+ */
 module.exports.updateEmployee = async (event) => {
     const employeeId = event.pathParameters.employeeId;
     const requestBody = JSON.parse(event.body);
     const { name, age, position } = requestBody;
+    console.log(`Name: ${name}, age: ${age}, position: ${position}`);
 
     const params = {
         TableName,
-        UpdateExpression: 'set age = :a, name = :n, position = :p',
+        UpdateExpression: 'SET #age = :a, #name = :n, #position = :p',
         ExpressionAttributeValues: {
-            a: age,
-            n: name,
-            p: position
+            ':a': age,
+            ':n': name,
+            ':p': position
         },
-        Key: employeeId
+        ExpressionAttributeNames: {
+            "#age": "age",
+            "#name": "name",
+            "#position": "position"
+        },
+        Key: { employeeId },
+        ReturnValues: 'UPDATED_NEW'
     };
 
+    console.log(`Params: ${JSON.stringify(params)}`);
+
     const result = await dynamoDb.update(params).promise();
+
     return httpReturn(
         HTTP_SUCCESS.SuccessOK,
         `Employee ${employeeId} succesfully updated`,
@@ -92,35 +121,50 @@ module.exports.updateEmployee = async (event) => {
     );
 };
 
+/**
+ * 
+ * @param {pathParameters.employeeId, body.age?, body.name?, body.position?} event 
+ * @returns httpReturn(statusCode, message, event)
+ * Updates a employee (optional properties) based on employeeId
+ */
 module.exports.patchEmployee = async (event) => {
     const employeeId = event.pathParameters.employeeId;
     const requestBody = JSON.parse(event.body);
 
     let UpdateExpression = '';
     let ExpressionAttributeValues = {};
+    let ExpressionAttributeNames = {};
 
     if (requestBody.age) {
-        UpdateExpression = 'set age =:a';
-        ExpressionAttributeValues.a = requestBody.age;
+        UpdateExpression = 'SET #age =:a';
+        ExpressionAttributeValues[':a'] = requestBody.age;
+        ExpressionAttributeNames["#age"] = "age";
     }
 
     if (requestBody.name) {
-        const newExpression = UpdateExpression.length > 0 ? ', set name =:n' : 'set name =:n';
-        UpdatedExpression = UpdateExpression.concat(newExpression);
-        ExpressionAttributeValues.n = requestBody.name;
+        const newExpression = UpdateExpression.length > 0 ? ', #name =:n' : 'SET #name =:n';
+        UpdateExpression = UpdateExpression.concat(newExpression);
+        ExpressionAttributeValues[':n'] = requestBody.name;
+        ExpressionAttributeNames["#name"] = "name";
     }
 
     if (requestBody.position) {
-        const newExpression = UpdateExpression.length > 0 ? ', set position =:p' : 'set position =:p';
-        UpdatedExpression = UpdateExpression.concat(newExpression);
-        ExpressionAttributeValues.p = requestBody.position;
+        const newExpression = UpdateExpression.length > 0 ? ', #position =:p' : 'SET #position =:p';
+        UpdateExpression = UpdateExpression.concat(newExpression);
+        ExpressionAttributeValues[':p'] = requestBody.position;
+        ExpressionAttributeNames["#position"] = "position";
     }
 
     const params = {
         TableName,
         UpdateExpression,
-        ExpressionAttributeValues
+        ExpressionAttributeValues,
+        ExpressionAttributeNames,
+        Key: { employeeId },
+        ReturnValues: 'UPDATED_NEW'
     }
+
+    console.log(`Params: ${JSON.stringify(params)}`);
 
     const result = await dynamoDb.update(params).promise();
 
@@ -131,10 +175,16 @@ module.exports.patchEmployee = async (event) => {
     );
 };
 
+/**
+ * 
+ * @param {pathParameters.employeeId} event 
+ * @returns httpReturn(statusCode, message, event)
+ * Deletes a employee based on employeeId
+ */
 module.exports.deleteEmployee = async (event) => {
     const employeeId = event.pathParameters.employeeId;
 
-    const result = await dynamoDb.delete({ TableName, Key: employeeId });
+    const result = await dynamoDb.delete({ TableName, Key: { employeeId } }).promise();
 
     return httpReturn(
         HTTP_SUCCESS.SuccessOK,
